cbcc489019877d678fce3a8b7589c796083a137e
valentin <valentin-42@mail.ru>
2016-07-16 16:57:09 +0300

    Эксперементы с потоками и выводом времени

--- RestedEyes/Form1.cs
+++ RestedEyes/Form1.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Threading;
 namespace RestedEyes
 {
     public partial class Form1 : Form
     {
+        public delegate void AddTime(String currTime);
+        public AddTime myDelegat;
+        private Thread mythread;
         public Form1()
         {
             InitializeComponent();
+            //ThreadTime();
+            myDelegat = new AddTime(RestartTime);
+            Treads();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -26,5 +32,20 @@ namespace RestedEyes
         {
             Autoloading.removeAutoloadingProgramm();
         }
+        public void RestartTime(String time)
+        {
+            label1.Text = time;
+        }
+        private void Treads()
+        {
+            mythread = new Thread(new ThreadStart(ThreadTime));
+            mythread.Start();
+        }
+        private void ThreadTime()
+        {
+            WachingTime time = new WachingTime(this);
+            time.Run();
+        }
+
     }
 }
---
+++ RestedEyes/WachingTime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace RestedEyes
+{
+    
+    class WachingTime
+    {
+        private  DateTime _localDate;
+        private  string _cultureName = "ru-RU";
+        private Form1 myform;
+
+        public WachingTime (Form1  form)
+        {
+            myform = form;
+        }
+        public  void Run()
+        {
+            while (true)
+            {
+                var culture = new CultureInfo(_cultureName);
+                _localDate = DateTime.Now;
+                myform.Invoke(myform.myDelegat, new Object[] { _localDate.ToString(culture) });
+            }
+        }
+    }
+}
