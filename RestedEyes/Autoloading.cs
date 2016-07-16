116dd5054f2e815b4260431b5c6e695ce7da292f
valentin <valentin-42@mail.ru>
2016-07-16 16:01:56 +0300

    Первый комит. сделано добвление в автозапуск

---
+++ RestedEyes/Autoloading.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace RestedEyes
+{
+    public class Autoloading
+    {
+        static string _userRoot = "HKEY_CURRENT_USER";
+        static string _subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+        static string _keyName = _userRoot + "\\" + _subKey;
+        static string _nameProgramm = "RestedEyes";
+
+        public static void addAutoloadingProgramm()
+        {
+                Registry.SetValue(_keyName,_nameProgramm, Application.ExecutablePath,RegistryValueKind.String);
+        }
+        public static void removeAutoloadingProgramm()
+        {
+            Registry.SetValue(_keyName, _nameProgramm, "", RegistryValueKind.String);
+        }
+    }
+}
