5cb4d5ffabd29b500731392d3708520e1bc576fe
valentin <valentin-42@mail.ru>
2016-09-20 22:08:42 +0300

    Баги после слияния

--- RestedEyes/Form1.cs
+++ RestedEyes/Form1.cs
@@ -24,6 +24,7 @@ namespace RestedEyes
             InitializeCurrentTimer();
 
             label2.Text = "";
+            button2.Text = "Отошел";
 
         }
         private void button1_Click(object sender, EventArgs e)
